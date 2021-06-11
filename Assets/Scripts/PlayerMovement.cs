b2abc1e258b6fe198a28cfdea17bd118f934b5fe
Patrick <patrickvyn@gmail.com>
2021-06-10 20:03:00 -0400

    Removed "PlayerMovement"
    it was a test script so I removed it in order to make a better one and not using the RigidBody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Rigidbody rb;
-    public float speed;
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W)){
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-        if(Input.GetKeyDown(KeyCode.S)){
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-
-        }if(Input.GetKeyDown(KeyCode.A)){
-
-            transform.Translate( speed * Time.deltaTime,0,0 );
-
-        }if(Input.GetKeyDown(KeyCode.D)){
-            transform.Translate(-speed * Time.deltaTime, 0,0 );
-        }
+        
     }
 }
